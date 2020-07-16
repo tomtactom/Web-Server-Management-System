399617661284488270c210acdfb1389d67b92ace
12tom12 <32821177+12tom12@users.noreply.github.com>
2020-07-16 21:31:07 +0200

    Update pre_installation.py

--- pre_installation.py
+++ pre_installation.py
@@ -1,1 +1,13 @@
+import os
+# Überprüfe ob der Nutzer Root Rechte hat
+if os.geteuid() == 0:
+    print('Please run this script with your standard user. Without sudo.')
+    quit()
 
+os.system('sudo pip install zipfile')
+os.system('sudo pip install urllib')
+os.system('sudo pip install string')
+os.system('sudo pip install random')
+os.system('sudo pip install shutil')
+with open('username.txt', 'w') as file:
+    file.write(str(os.system('whoami')))
