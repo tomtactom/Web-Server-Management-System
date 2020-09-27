7b9c9d5e53d6295592c411b04e8ed07f91a85372
12tom12 <32821177+12tom12@users.noreply.github.com>
2020-09-27 16:04:34 +0200

    test

--- httpd/webhook.php
+++ httpd/webhook.php
@@ -23,8 +23,9 @@ if (is_dir($verzeichnis)) {
               echo '<code>cd '.$verzeichnis.'/'.$subdomain_data[1].'/'.$docs.' ; git fetch --all ; git reset --hard origin/master ; git pull origin master</code>';
               echo shell_exec('cd '.$verzeichnis.'/'.$subdomain_data[1].'/'.$docs.' ; git fetch --all ; git reset --hard origin/master ; git pull origin master');
               echo shell_exec('chmod -R 777 '.$verzeichnis.'/'.$subdomain_data[1].'/'.$docs);
-              echo $file;
-              if($file == 'webservermanagementsystem') {
+              echo '"'.$file.'"';
+              echo $subdomain_data[1].'"'
+              if($subdomain_data[1] == 'webservermanagementsystem') {
                 echo shell_exec('sudo service webservermanagementsystem start');
                 echo 'hallo';
               }
